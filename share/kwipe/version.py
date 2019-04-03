c5af7704fff60bcf191d0dfc886824e9d7b1a1dc
PyCoder <fabian.dimilia@gmail.com>
2019-04-04 01:57:35 +0200

    Update version.py

--- share/kwipe/version.py
+++ share/kwipe/version.py
@@ -17,5 +17,5 @@
 #
 # Author(s): Fabian Di Milia <fabian.dimilia@gmail.com>
 
-VERSION = '2.2.4'
-URL = 'https://raw.githubusercontent.com/PyCoder/KWipe/master/release.md'
+VERSION = '2.2.5'
+URL = 'https://raw.githubusercontent.com/PyCoder/KWipe/master/RELEASE'
