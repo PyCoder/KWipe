9382f23bd024bb9270828dce806cbf6648558381
PyCoder <1201123+PyCoder@users.noreply.github.com>
2020-05-23 18:34:12 +0200

    Update about.py

--- share/kwipe/about.py
+++ share/kwipe/about.py
@@ -1,6 +1,6 @@
 # about.py
 #
-# Copyright (C) 2012 - 2019 Fabian Di Milia, All rights reserved.
+# Copyright (C) 2012 - 2020 Fabian Di Milia, All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
